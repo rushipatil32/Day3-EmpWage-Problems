d45b02a4a04689503c7a428fe2468d8638b52edc
Rushikesh Patil <rushipatil6632@gmail.com>
2022-02-07 00:29:25 +0530

    Added:addition of part time

--- src/com/employeewage/EmployeeWageComputation.java
+++ src/com/employeewage/EmployeeWageComputation.java
@@ -2,19 +2,24 @@ package com.employeewage;
 
 public class EmployeeWageComputation {
 public static void main(String[] args) {
-	int isFULL_TIME = 1;
+	int isPART_TIME = 1;
+    int isFULL_TIME = 2;
     int Emp_Rate_Per_Hr = 20;
-    int empcheck = (int) (Math.random() * 100) % 2;
+    int empcheck = (int) (Math.random() * 100) % 3;
     int workingHours = 0;
     if (empcheck == isFULL_TIME)
-    
-        System.out.println("Employee is Present");
+    {
+        System.out.println("Employee is Present Full time");
         workingHours = 8;
-    
+    } else if (empcheck == isPART_TIME)
+    {
+        System.out.println("Employee is Present Part time");
+        workingHours = 4;
+    } else
     
         System.out.println("Employee is Absent");
     
-    int dailywage = workingHours * Emp_Rate_Per_Hr;
+    int wage = workingHours * Emp_Rate_Per_Hr;
     System.out.println("Employee Daily Wage is " + wage);
 	}
 }
