327efcaf79ca657bccd19fc01990193354d955eb
Rushikesh Patil <rushipatil6632@gmail.com>
2022-02-07 00:15:45 +0530

    Employee Dailywage

--- src/com/employeewage/EmployeeWageComputation.java
+++ src/com/employeewage/EmployeeWageComputation.java
@@ -3,16 +3,18 @@ package com.employeewage;
 public class EmployeeWageComputation {
 public static void main(String[] args) {
 	int isFULL_TIME = 1;
-    double empcheck = (int) (Math.random() * 100) % 2;
+    int Emp_Rate_Per_Hr = 20;
+    int empcheck = (int) (Math.random() * 100) % 2;
+    int workingHours = 0;
     if (empcheck == isFULL_TIME)
     
         System.out.println("Employee is Present");
-
-     else
+        workingHours = 8;
+    
     
         System.out.println("Employee is Absent");
-
-    }
+    
+    int dailywage = workingHours * Emp_Rate_Per_Hr;
+    System.out.println("Employee Daily Wage is " + wage);
+	}
 }
-
-
