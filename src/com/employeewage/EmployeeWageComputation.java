8fab617c60732ce97f631984d7b7de9fadf4dca0
Rushikesh Patil <rushipatil6632@gmail.com>
2022-02-07 16:35:17 +0530

    Added:Solving using swith case

--- src/com/employeewage/EmployeeWageComputation.java
+++ src/com/employeewage/EmployeeWageComputation.java
@@ -2,23 +2,24 @@ package com.employeewage;
 
 public class EmployeeWageComputation {
 public static void main(String[] args) {
-	int isPART_TIME = 1;
-    int isFULL_TIME = 2;
-    int Emp_Rate_Per_Hr = 20;
-    int empcheck = (int) (Math.random() * 100) % 3;
-    int workingHours = 0;
-    if (empcheck == isFULL_TIME)
+	final int isPART_TIME = 1;
+    final int isFULL_TIME = 2;
+    final int Emp_Rate_Per_Hr = 20;
+    int empCheck = (int) (Math.random() * 100) % 3;
+    int workingHours =0;
+    switch (empCheck)
     {
-        System.out.println("Employee is Present Full time");
-        workingHours = 8;
-    } else if (empcheck == isPART_TIME)
-    {
-        System.out.println("Employee is Present Part time");
-        workingHours = 4;
-    } else
-    
-        System.out.println("Employee is Absent");
-    
+        case isFULL_TIME:
+            System.out.println("Employee is Present Full time");
+            workingHours = 8;
+            break;
+        case isPART_TIME:
+            System.out.println("Employee is Present Part time");
+            workingHours = 4;
+            break;
+        default:
+            System.out.println("Employee is Absent");    
+    }
     int wage = workingHours * Emp_Rate_Per_Hr;
     System.out.println("Employee Daily Wage is " + wage);
 	}
